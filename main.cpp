8f3f9ad3b6d317ea5096366a94649ca3154a85f8
llllIlIlIlll <aniriloris11@gmail.com>
2024-11-06 16:36:13 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 bool isPrime(int num) {
@@ -17,6 +18,85 @@ int sumOfDigits(int num) {
         num /= 10;
     }
     return sum;
+}
+
+long long O10n(int N) { // O(10^n) brute force
+    int max_sum = 9 * N + 1;
+    vector<int> D(max_sum, 0);
+
+    for (int x = 0; x < pow(10, N); ++x) {
+        D[sumOfDigits(x)]++;
+    }
+
+    long long L_n = 0;
+    for (int y = 0; y < max_sum; ++y) {
+        L_n += D[y] * D[y];
+    }
+
+    return L_n;
+}
+
+long long On2(int n) { // O(n^2) dynamic programming
+    int max_sum = 9 * n;
+    vector<vector<int>> D(n + 1, vector<int>(max_sum + 1, 0));
+    D[0][0] = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int k = 0; k <= max_sum; ++k) {
+            for (int j = 0; j < 10; ++j) {
+                if (k - j >= 0) {
+                    D[i][k] += D[i - 1][k - j];
+                }
+            }
+        }
+    }
+
+    long long L_n = 0;
+    for (int k = 0; k <= max_sum; ++k) {
+        L_n += static_cast<long long>(D[n][k]) * D[n][k];
+    }
+
+    return L_n;
+}
+
+long long On(int N) { // O(???)
+    int mx_sm = 9 * N;
+    vector<int> D(mx_sm + 1, 0);
+    D[0] = 1;
+
+    vector<int> incrY(N, 0);
+    incrY[0] = 1;
+    incrY[1] = 1 - N;
+    for (int i = 2; i < N; ++i) {
+        incrY[i] = -incrY[i - 1] * N / i + incrY[i - 1];
+    }
+
+    vector<int> incrA(N - 2, 0);
+    for (int i = 0; i < N - 2; ++i) {
+        incrA[i] = N - i - 1;
+    }
+
+    int delta = 0;
+    for (int i = 1; i <= mx_sm / 2 + 1; ++i) {
+        D[i] = D[i - 1] + incrA[0];
+        incrA.back() += incrY[(i - delta) / 9];
+
+        delta += (i % 10 == 0);
+        for (int j = N - 3; j > 0; --j) {
+            incrA[j - 1] += incrA[j];
+        }
+    }
+
+    for (int j = mx_sm / 2; j <= mx_sm; ++j) {
+        D[j] = D[mx_sm - j];
+    }
+
+    long long L_n = 0;
+    for (int k = 0; k <= mx_sm; ++k) {
+        L_n += static_cast<long long>(D[k]) * D[k];
+    }
+
+    return L_n;
 }
 
 
@@ -40,47 +120,61 @@ int main(){
 
 
     // dop 2
-    // int luckyTicketCount = 0;
-
-    // for (int ticket = 0; ticket < 1'000'000; ++ticket) {
-    //     int tmp = ticket;
-    //     vector<int> x(6);
-    //     for(int i=0; i<6; ++i){
-    //         x[i] = (tmp%10);
-    //         tmp /= 10;
-    //     }
-        
-    //     int l = 0, r = 0;
-    //     for(int i=0; i<6; ++i){
-    //         if (i<3) r += x[i];
-    //         else l += x[i];
-    //     }
-    //     if(l==r) luckyTicketCount++;
-    // }
-
-    // cout << luckyTicketCount << endl;
+    int N = 3;
+    cout << O10n(N) << ' ' << On2(N) << ' ' << On(N) << endl;
+    // cout << On2(N) << ' ' << On(N) << endl;
 
 
     // dop 3
-    // cout << "Enter N - number of numbers in sequence" << endl;
-    int N = 3;
-    // int subsequence[N];
-    // for(int i=0; i<N-1; ++i) cin >> subsequence[i];
+    // int j = 5;
+    // vector<int> A = {2,767,77,777,3};
 
-    int subsequence[6] = {78,4,5};
-    for(int i=0; i<N; ++i) cout << subsequence[i] << ' ';
+    // for(int i=0;i<j;i++) cout << A[i] << ' ';
+    // cout << endl;
+
+    // int Δ = 0;
+    // for(int x=0; x<j; ++x)
+    // {
+    //     if (isPrime(A[x])) ++Δ; 
+    //     else 
+    //     {
+    //         A[x-Δ] = A[x];
+    //         if (sumOfDigits(A[x]) == 15) 
+    //         { 
+    //             A.insert(A.begin()+x, A[x]); ++j; ++x; A[x-Δ] = A[x]; 
+    //         }
+    //     }
+    // }
+    // j -= Δ;
+    // A.resize(j);
+
+    // for(int i=0;i<j;i++) cout << A[i] << ' ';
+
+    int j = 5;
+    int* A = new int[j]{ 3, 78, 77, 777 , 3};
+
+    for(int i=0;i<j;i++) cout << A[i] << ' ';
     cout << endl;
 
-    int offset = 0;
-    for (int i=0; i<3-offset; ++i){
-        if(isPrime(subsequence[i])) { --offset; }
-        else if(sumOfDigits(subsequence[i]) == 15) { subsequence[i+offset] = subsequence[i]; subsequence[N] = subsequence[i]; ++N; }
-        else { subsequence[i+offset] = subsequence[i]; }
-        // cout << subsequence[i] << (isPrime(subsequence[i]));
+    int Δ = 0;
+    int count15 = 0;
+    for(int x=0; x<j; ++x)
+    {
+        if (isPrime(A[x])) ++Δ; 
+        else 
+        {
+            A[x-Δ] = A[x];
+            if (sumOfDigits(A[x]) == 15) ++count15;
+        }
+    }
+    j += count15-Δ;
+
+    for(int x=j-1; x>=0; --x){
+        if (sumOfDigits(A[x]) == 15) { A[x+count15] = A[x]; --count15; }
+        A[x+count15] = A[x];
     }
 
-
-    for(int i=0; i<N; ++i) cout << subsequence[i] << ' ';
+    for(int x=0; x<j; ++x) cout << A[x] << ' ';
 
 
 
